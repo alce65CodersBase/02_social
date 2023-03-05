0414c136ed34ef1a641160144d0b15689413561f
Alejandro Cerezo <alce65@hotmail.es>
2023-03-05 13:28:16 +0100

    ✨ Add environment selection when connect db and test it

--- api/src/db/db.connect.test.ts
+++ api/src/db/db.connect.test.ts
@@ -1,8 +1,21 @@
 import { dbConnect } from './db.connect';
 import mongoose from 'mongoose';
 
-test('should first', async () => {
-  const result = await dbConnect();
-  expect(typeof result).toBe(typeof mongoose);
-  mongoose.disconnect();
+describe('Given dbConnect function', () => {
+  describe('When NODE_ENV === test', () => {
+    test('Then it should be a connection to testing db', async () => {
+      const result = await dbConnect();
+      expect(typeof result).toBe(typeof mongoose);
+      expect(mongoose.connection.db.databaseName).toContain('Testing');
+      mongoose.disconnect();
+    });
+  });
+  describe('When NODE_ENV !== test', () => {
+    test('Then it should be a connection to testing db', async () => {
+      const result = await dbConnect('dev');
+      expect(typeof result).toBe(typeof mongoose);
+      expect(mongoose.connection.db.databaseName).not.toContain('Testing');
+      mongoose.disconnect();
+    });
+  });
 });
