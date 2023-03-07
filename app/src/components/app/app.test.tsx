2671b98d82d154152e29d6a20c8126dd8a490641
Alejandro Cerezo <alce65@hotmail.es>
2023-03-07 11:45:43 +0100

    🚚 Reorganize initial code

--- app/src/components/app/app.test.tsx
+++ app/src/components/app/app.test.tsx
@@ -1,6 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
-import App from './App';
+import { App } from './app';
 describe('Given App', () => {
   describe('When', () => {
     test('Then it should ...', () => {
